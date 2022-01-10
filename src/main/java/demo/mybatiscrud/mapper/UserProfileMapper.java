0be4b26b1f16de74505c7617c5f237a3364eb9d1
JungTag <dbsdudxor121@naver.com>
2022-01-11 00:27:14 +0900

    [feat] annotation mapper 추가

---
+++ src/main/java/demo/mybatiscrud/mapper/UserProfileMapper.java
@@ -0,0 +1,34 @@
+package demo.mybatiscrud.mapper;
+
+import demo.mybatiscrud.model.UserProfile;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface UserProfileMapper {
+    @Select("SELECT * FROM UserProfile WHERE id=#{id}")
+    UserProfile getUserProfile(@Param("id") String id);
+
+    @Select("SELECT * FROM UserProfile")
+    List<UserProfile> getUserProfileList();
+
+    @Insert("INSERT INTO UserProfile VALUES(#{id}, #{name}, #{phone}, #{address})")
+    int insertUserProfile(
+            @Param("id") String id,
+            @Param("name") String name,
+            @Param("phone") String phone,
+            @Param("address") String address
+    ); // affected row가 반환됨
+
+    @Update("UPDATE UserProfile SET name=#{name}, phone=#{phone}, address=#{address} WHERE id=#{id}")
+    int updateUserProfile(
+            @Param("id") String id,
+            @Param("name") String name,
+            @Param("phone") String phone,
+            @Param("address") String address
+    );
+
+    @Delete("DELETE FROM UserProfile WHERE id=#{id}")
+    int deleteUserProfile(@Param("id") String id);
+}
